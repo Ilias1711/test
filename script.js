391f2d93476736f1515d89e06722a9cf0642d3fe
Ilias1711 <118740676+Ilias1711@users.noreply.github.com>
2024-07-12 12:04:38 +0500

    newTest

---
+++ script.js
@@ -0,0 +1,11 @@
+// let items = document.querySelectorAll(".item");
+
+// setInterval(function remove() {
+//   Array.from(items).forEach((elem, index) => {
+//     if (index == 0 || index == 2 || index == 4) {
+//       elem.classList.toggle("item__color__blue");
+//     } else if (index == 1 || index == 3 || index == 5) {
+//       elem.classList.toggle("item__color__green");
+//     }
+//   });
+// }, 1500);
